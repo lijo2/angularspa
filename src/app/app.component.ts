47f600f0a213e851fc14fee00449e6ebba45f5d8
lijo <Jonathan_Li@wsib.on.ca>
2020-07-07 14:01:49 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularspa';
+  textField = new FormControl('');
+
+  onClick() {
+    console.log('call backend api to get data');
+    console.log(this.textField.value);
+    this.textField.setValue( 'new value');
+  }
+
+  onLogin() {
+    console.log('start user login');
+  }
 }
