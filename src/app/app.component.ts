debe17f780edb7b1aaa2ed9c0f38e29108abe842
lijo <Jonathan_Li@wsib.on.ca>
2020-07-19 23:47:13 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,68 @@
 import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+import { HttpClient, HttpClientJsonpModule } from '@angular/common/http';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularspa';
   textField = new FormControl('');
+  titleField = new FormControl('');
+  resultField = new FormControl('');
 
-  constructor(private http: HttpClient) {}
+  serviceUrl: string;
+
+  constructor(private http: HttpClient) {
+    if (this.isOnLocalHost()) {
+      this.serviceUrl = 'http://localhost:8080';
+    } else {
+      this.serviceUrl = 'https://springapitestli.azurewebsites.net';
+    }
+  }
+
+  isOnLocalHost() {
+    return window.location.host.startsWith('localhost');
+  }
 
   onClick() {
     console.log('call backend api to get data');
     const str = this.textField.value;
-    this.http.get('https://springapitestli.azurewebsites.net/jsonapi/' + str).subscribe((resp) => {
-      this.textField.setValue(JSON.stringify(resp));
+    this.http.get(this.serviceUrl + '/jsonapi/' + str).subscribe((resp) => {
+      this.resultField.setValue(JSON.stringify(resp));
     });
+  }
+
+  onClickStorage(id) {
+    if (id === 0) {
+      const data = {
+        name: this.titleField.value,
+        data: this.textField.value,
+      };
+
+      this.http.post(this.serviceUrl + '/storages/', data).subscribe((resp) => {
+        this.resultField.setValue(resp);
+      });
+    } else if (id === 1) {
+      this.http.get(this.serviceUrl + '/storages/').subscribe((resp) => {
+        this.resultField.setValue(resp);
+      });
+    } else if (id === 2) {
+      this.http
+        .delete(this.serviceUrl + '/storages/' + this.titleField.value)
+        .subscribe((resp) => {
+          this.resultField.setValue(resp);
+        });
+    } else if (id === 3) {
+      this.http
+        .get(this.serviceUrl + '/storages/' + this.titleField.value, {responseType: 'text'})
+        .subscribe((resp) => {
+          this.resultField.setValue(resp);
+        });
+    }
   }
 
   onLogin() {
