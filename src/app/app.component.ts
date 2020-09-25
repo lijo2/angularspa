24de9ea92e9fbb13753b4977d15c43a5630fcdf1
lijo <Jonathan_Li@wsib.on.ca>
2020-09-25 15:28:49 -0400

    update for app insights support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { FormControl } from '@angular/forms';
 import { HttpClient, HttpClientJsonpModule } from '@angular/common/http';
 import { ActivatedRouteSnapshot } from '@angular/router';
 import { ApplicationInsightsService } from './applicationInsights.service';
+import { ThrowStmt } from '@angular/compiler';
+import { SeverityLevel } from '@microsoft/applicationinsights-web';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,11 @@ import { ApplicationInsightsService } from './applicationInsights.service';
 export class AppComponent implements OnInit {
   title = 'angularspa';
   titleField = new FormControl('title');
-  textField = new FormControl('data');
+  dataField = new FormControl('data');
   resultField = new FormControl('');
 
   serviceUrl: string;
+  private count = 0;
 
   constructor(private http: HttpClient, public applicationInsightsService: ApplicationInsightsService) {
     if (this.isOnLocalHost()) {
@@ -48,17 +51,100 @@ export class AppComponent implements OnInit {
   }
 
   onTempTest() {
-    let m = this.hello;
+  /*  let m = this.hello;
     if (this.textField.value) {
       m = null;
     }
-    m.call(this);
+    m.call(this);*/
+    const data = this.dataField.value;
+    const title = this.titleField.value;
+    switch (title) {
+      case 'event': {
+        this.applicationInsightsService.logEvent(
+          title, {myValue: data, myDate: Date.now()}
+        );
+        break;
+      }
+      case 'metric': {
+        this.applicationInsightsService.logMetric(
+          'supportingdocnum', this.count++, {myValue: data, myDate: Date.now()}
+        );
+        break;
+      }
+      case 'pageview': {
+        this.applicationInsightsService.logPageView(
+          'pageview' + (this.count++).toString(), window.location.href
+        );
+        break;
+      }
+      case 'xhr': {
+        try {
+          this.http.get(data, {responseType: 'text'}).subscribe( (dat: any) => {
+            console.log(dat);
+            this.applicationInsightsService.logTrace('next http callback' + dat, {myValue: data, myDate: Date.now()});
+          }, (err) => {
+            this.applicationInsightsService.logTrace('err http callback', {myName: err.name, myDate: err.message});
+            this.applicationInsightsService.logException(err);
+          },
+          () => {
+            console.log('http complete handler');
+            this.applicationInsightsService.logTrace('complete http callback', {myValue: data, myDate: Date.now()});
+          });
+        } catch (ex) {
+          this.applicationInsightsService.logTrace('try catch http callback', {myValue: data, myDate: Date.now()});
+          this.applicationInsightsService.logException(ex);
+        }
+        break;
+      }
+      case 'exception': {
+        if (data === 'caught') {
+          try {
+            let m = this.hello;
+            if (this.titleField.value) {
+              m = null;
+            }
+            m.call(this);
+          } catch (ex) {
+            this.applicationInsightsService.logException(ex);
+          }
+        } else {
+            let m = this.hello;
+            if (this.titleField.value) {
+              m = null;
+            }
+            m.call(this);
+        }
+        break;
+      }
+      case 'log': {
+        if (data === 'info') {
+          this.applicationInsightsService.logTrace('write log of info level', {myValue: data, myDate: Date.now()},
+            SeverityLevel.Information);
+        } else if (data === 'warning') {
+          this.applicationInsightsService.logTrace('write log of warn level', {myValue: data, myDate: Date.now()},
+          SeverityLevel.Warning);
+        } else if (data === 'error') {
+          this.applicationInsightsService.logTrace('write log of error level', {myValue: data, myDate: Date.now()},
+          SeverityLevel.Error);
+        } else if (data === 'critical') {
+          this.applicationInsightsService.logTrace('write log of critical level', {myValue: data, myDate: Date.now()},
+          SeverityLevel.Critical);
+        } else {
+          this.applicationInsightsService.logTrace('write log of verbose level', {myValue: data, myDate: Date.now()},
+          SeverityLevel.Verbose);
+        }
+        break;
+      }
+      default: {
+        this.applicationInsightsService.logTrace('switch log for unmatched action: ' + title, {myValue: data, myDate: Date.now()});
+      }
+    }
   }
 
   onTempTest2() {
     try {
       let m = this.hello;
-      if (this.textField.value) {
+      if (this.dataField.value) {
         m = null;
       }
       m.call(this);
@@ -80,7 +166,7 @@ export class AppComponent implements OnInit {
     if (id === 0) {
       const data = {
         name: this.titleField.value,
-        data: this.textField.value,
+        data: this.dataField.value,
       };
 
       this.http.post(this.serviceUrl + '/storages/', data).subscribe((resp) => {
--- src/app/applicationInsights.service.ts
+++ src/app/applicationInsights.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ApplicationInsights, SeverityLevel } from '@microsoft/applicationinsights-web';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -22,6 +22,9 @@ export class ApplicationInsightsService {
     this.appInsights.loadAppInsights();
   }
 
+  // pageview can be viewed from app insight's log and Event
+  // call s logPageView(
+  // logPageView(pageName, pageUrl);
   logPageView(name?: string, url?: string) { // option to call manually
     this.appInsights.trackPageView({
       name,
@@ -29,20 +32,35 @@ export class ApplicationInsightsService {
     });
   }
 
+  // event log can be viewed from app insight's Log or Search for CustomEvent
+  // each logEvent call will show as a separate item
+  // call sample:
+  // logEvent(eventTitle, {myValue: data, myDate: Date.now()});
   logEvent(name: string, properties?: { [key: string]: any }) {
-    this.appInsights.trackEvent({ name}, properties);
+    this.appInsights.trackEvent({name}, properties);
   }
 
+  // metric log can be viewed from app insight's Metric blade
+  // it calculages and shows totol call count, max, min, average, and sum
+  // call sample:
+  // logMetric('docsize', file.length, {filename: file.name, uploadDate: Date.now()}
   logMetric(name: string, average: number, properties?: { [key: string]: any }) {
     this.appInsights.trackMetric({ name, average }, properties);
   }
 
+  // exception log can be viewed from app insight's Search or Failure page
+  // Call this method to log error or exception. For uncaught exception,
+  // ErrorHandler service will catch exception and call this method to log it
+  // call sample:
+  // logException(ex);
   logException(exception: Error, severityLevel?: number) {
     this.appInsights.trackException({ exception, severityLevel });
   }
 
-  logTrace(message: string, properties?: { [key: string]: any }) {
-    this.appInsights.trackTrace({ message}, properties);
+  // log trace can be viewed from Log or Search page
+  // call sample:
+  // logTrace('write log of warn level', {myValue: data, myDate: Date.now()}, SeverityLevel.Warning);
+  logTrace(message: string, properties?: { [key: string]: any }, severityLevel?: SeverityLevel) {
+    this.appInsights.trackTrace({message, severityLevel}, properties);
   }
-
 }
